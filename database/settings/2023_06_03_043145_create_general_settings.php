2ec7a8369a7f02239b911354a653d77c774c85d6
santhana-murugesh <sarjunvfc0801@gmail.com>
2025-07-15 01:05:33 +0530

    new

--- database/settings/2023_06_03_043145_create_general_settings.php
+++ database/settings/2023_06_03_043145_create_general_settings.php
@@ -8,13 +8,13 @@ class CreateGeneralSettings extends SettingsMigration
     {
         $this->migrator->add('general.global_discount', 50);
         $this->migrator->add('general.min_order_value', 2000);
-        $this->migrator->add('general.company_name', 'ps');
+        $this->migrator->add('general.company_name', 'Rajamanis pyrotech');
                     $this->migrator->add('general.email', 'info@vairamcrackers.com');
         $this->migrator->add('general.company_address', '1 st ');
         $this->migrator->add('general.mobile_number', [
-            ['Name' => 'Order Confirmation', 'Number' => '93843 57572,  80569 03599, 73971 53601, 97504 44600, 75388 97572'],
-            ['Name' => ' Payment Confirmation', 'Number' => '97905 72926, 97900 39111, 97900 25111, 63854 27540, 63854 27539'],
-            ['Name' => 'Complaint', 'Number' => '9942449196, 7540011846, 7530027560, 7530034370']
+            ['Name' => 'Order Confirmation', 'Number' => '9677879734'],
+            ['Name' => ' Payment Confirmation', 'Number' => '9677879734'],
+            ['Name' => 'Complaint', 'Number' => '9677879734']
         ]);
     }
 }
