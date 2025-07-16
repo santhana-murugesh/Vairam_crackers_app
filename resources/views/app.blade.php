2c05f83cf673371142946597a591fb638092d44b
santhana-murugesh <sarjunvfc0801@gmail.com>
2025-07-16 09:49:40 +0530

    favi

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Vairam Crackers') }} - Buy Best quality fire crackers from Sivakasi</title>
+        <title>{{ config('app.name', ) }} - Buy Best quality fire crackers from Sivakasi</title>
 
         <link rel="icon" type="image/x-icon" href="image/logo/logo-1.png">
         <link rel="stylesheet" href="/css/style.css">
